ac90f359548b1a6f028ccba7404d70e8194ef98f
SMK-DEV <samuel.freitas@fabricadeaplicativos.com.br>
2022-03-24 23:57:38 -0300

    Feature: endpoint /users e /coworking

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-import express from 'express';
+const express = require('express');
+
+const UserController = require('./lib/users/user-controller');
+const CoworkingController = require('./lib/coworking/coworking-controller');
+
+
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(express.json())
+
+app.get('/users', UserController.getUsers);
+app.post('/users', UserController.createUser);
+app.put('/users/:id', UserController.updateUser);
+app.delete('/users/:id', UserController.deleteUser);
+
+app.get('/coworking', CoworkingController.getCoworking);
+app.post('/coworking', CoworkingController.createCoworking);
+app.put('/coworking/:id', CoworkingController.updateCoworking);
+app.delete('/coworking/:id', CoworkingController.deleteCoworking);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
